ca2aa7b78696e570fc186ea4e351d0b686e49f34
selvijebucolli1 <selvije.bucoli87@gmail.com>
2022-11-10 19:57:28 +0100

    .

---
+++ main.js
@@ -0,0 +1,90 @@
+// let number= prompt("Number to square?");
+// let squaredNumber=number*number;
+
+// console.info(squaredNumber);
+
+//2
+// let numberA= prompt("First number")*1;
+// let numberB= prompt("Second number")*1;
+// console.log((numberA + numberB )/2);
+
+//3
+// let square= prompt("Add side length")*1;
+// console.log(square *4);
+
+//4
+//Execute a km to mile converter. 1 km = 0,621371 miles. State this as a constant in the code.
+// const mile = 0.621371;
+
+// let km =prompt("Km to mile convertor") *1;
+// console.log(km*mile);
+
+//5. // Execute a calculator. A user types in two numbers and the program puts out the results of + - * / actions with those numbers.
+// let numberA= prompt("First number")*1;
+// let numberB= prompt("Second number")*1;
+// console.log(numberA + numberB);
+// console.log(numberA - numberB);
+// console.log(numberA * numberB);
+// console.log(numberA / numberB);
+
+//6. A user types in the values of a and b for a * x + b = 0 formula and the program counts the x value.
+// let a=prompt("Enter value a:")*1;
+// let b=prompt("Enter value b:")*1;
+// console.log(-b/a);
+
+//7.// An employee’s monthly paycheck is $250 + 10% from sales. Request the sum of all monthly sales and count the salary.
+// let sales = prompt("Enter the sum of monthly sales: ")* 1;
+// let salesyear = ((sales * 12) * 0.1) + (250*0.1);
+// console.log(salesyear);
+
+
+//------------------------
+//1.Request a user’s name and display the response “Hello, [name]”.
+// let username = prompt('Enter your name: ');
+// let cookie= `Hello, ${username}`;
+// console.info(cookie);
+
+
+//2.Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+// let year = prompt("Enter birth year ")* 1;
+// const currentyear=2022;
+// console.log(currentyear-year);
+
+//3.Request a length of a side of a square from a user and display the perimeter of the square.
+// let square= prompt("Enter side length: ")*1;
+// console.log(square * 4);
+
+//4.Request a radius of a circle and display the area of such a circle.
+// let radius=prompt("Enter radius:")*1;
+// const PI=3.14159265359;
+// circleArea=PI * (radius * radius)
+// console.log(circleArea)
+
+//5.Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+// let distance = prompt("Enter distance between the two cities in km: ")*1;
+// let time = prompt("Enter the time needed to be there in hours:")*1;
+// let speed = distance/time;
+// console.log(speed);
+
+//6.Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+//  const euro = 0.99;
+//  let usd =prompt("usd to euro convertor") *1;
+//  console.log(usd*euro);
+
+//7.A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+// let GB = prompt("Enter the flash drive memory in GB: ")*1;
+// let MB = GB*1000/820;
+// console.log(MB);
+
+//8.A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+// let wallet=prompt("Enter amount in wallet:")*1;
+// let chocoPrice=prompt("Enter chocolate price:")*1;
+// let howManyChocos=wallet/chocoPrice;
+// let chocoAmount= `The number of chocolates you can buy ${howManyChocos}`;
+// let change= `You have ${wallet-(howManyChocos*chocoPrice)} change left`;
+// console.info(chocoAmount + change);
+
+//9.The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+// let deposit = prompt("Enter the sum of bank deposits for 2 months: ")*1;
+// let suminterest =deposit*(5/100)*2;
+
